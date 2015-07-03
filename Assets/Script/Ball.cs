3a8f08c9fb7f2da3dd67ad3b0247ab020e96fc06
Isuru Udana Palihawadana <isuruudana@gmail.com>
2015-07-03 13:34:52 +0530

    add

---
+++ Assets/Script/Ball.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+	public float speed;
+	public Rigidbody body;
+	public Vector3 tilt;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.Translate(Input.acceleration.x/2, Input.acceleration.y/2,0);
+		 
+		tilt.z = Input.acceleration.y;
+		tilt.x = Input.acceleration.x;
+		body.AddForce(tilt*speed*Time.deltaTime);
+
+	}
+}

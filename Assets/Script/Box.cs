25a1e26a33d66bd9c73c9c3495c9967916ddd02e
Isuru Udana Palihawadana <isuruudana@gmail.com>
2015-07-05 17:10:19 +0530

    update

---
+++ Assets/Script/Box.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Box : MonoBehaviour {
+	public Vector3 tilt;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//tilt.y = Input.acceleration.y;
+		//tilt.x = Input.acceleration.x;
+		tilt.z = Input.acceleration.y;
+		transform.Rotate(tilt);
+	}
+
+
+}
